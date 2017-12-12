901450e626f97e49c9cd377df8228efa31528044
Ajith Appukuttan <withajith@gmail.com>
2017-12-11 21:20:09 -0500

    added HelloWorld.js

---
+++ HelloWorld.js
@@ -0,0 +1,11 @@
+console.log("My First Baby Step");
+
+const notes=require('./notes.js')
+
+const _=require('lodash');
+var x= 10;
+if (x===15) {
+    console.log("My First condition");
+} else if (x==10) {
+      console.log("Else  First condition");
+}
